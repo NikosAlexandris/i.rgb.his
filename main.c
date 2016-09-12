f02b896e938c1fef9fa53d72abc429826f638781
Nikos Alexandris <nik@nikosalexandris.net>
2016-09-12 03:07:55 +0200

    spell out variable names, introduced bits option

--- main.c
+++ main.c
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <grass/gis.h>
 #include <grass/raster.h>
 #include <grass/glocale.h>
@@ -28,7 +29,7 @@
 
 int main(int argc, char **argv)
 {
-    unsigned int bands;
+    unsigned int band;
     unsigned long row;
     unsigned int rows;
     unsigned int cols;
@@ -39,12 +40,14 @@ int main(int argc, char **argv)
     struct Option *opt_hue;
     struct Option *opt_intensity;
     struct Option *opt_saturation;
+    struct Option *opt_bits;
     struct GModule *module;
     int fd_input[3];
     int fd_output[3];
+    int bits;   // bitness of input raster maps
+    double max_colors;  // maximum level based on input bitness
 
-    /* Initialize GIS engine */
-    G_gisinit(argv[0]);
+    G_gisinit(argv[0]);  // initialize GIS engine
 
     /* Set description */
     module = G_define_module();
@@ -74,35 +77,52 @@ int main(int argc, char **argv)
     opt_hue->key = "hue";
     opt_hue->description = _("Name for output raster map (hue)");
 
-    opt_inten = G_define_standard_option(G_OPT_R_OUTPUT);
-    opt_inten->key = "intensity";
-    opt_inten->description = _("Name for output raster map (intensity)");
+    opt_intensity = G_define_standard_option(G_OPT_R_OUTPUT);
+    opt_intensity->key = "intensity";
+    opt_intensity->description = _("Name for output raster map (intensity)");
 
-    opt_sat = G_define_standard_option(G_OPT_R_OUTPUT);
-    opt_sat->key = "saturation";
-    opt_sat->description = _("Name for output raster map (saturation)");
+    opt_saturation = G_define_standard_option(G_OPT_R_OUTPUT);
+    opt_saturation->key = "saturation";
+    opt_saturation->description = _("Name for output raster map (saturation)");
+
+    opt_bits = G_define_option();
+    opt_bits->key = "bits";
+    opt_bits->type = TYPE_INTEGER;
+    opt_bits->required = NO;
+    opt_bits->answer = "8";
+    opt_bits->options = "2-16";
+    opt_bits->description = _("Bits per input image");
 
     if (G_parser(argc, argv))
         exit(EXIT_FAILURE);
+
+    /* bit depth, should be > 0 */
+    bits = atoi(opt_bits->answer);
+    if (bits <= 0)
+        G_fatal_error(_("Invalid bit depth definition!"));
+
+    /* open half ended range for maximum level */
+    max_colors = pow(2, bits) - 1.0;
+    G_debug(1, "%d-bit data ranging in [0,%.0f)", bits, max_colors);
 
     /* get dimension of the image */
     rows = Rast_window_rows();
     cols = Rast_window_cols();
 
     openfiles(opt_red->answer, opt_green->answer, opt_blue->answer,
-            opt_hue->answer, opt_inten->answer, opt_sat->answer,
+            opt_hue->answer, opt_intensity->answer, opt_saturation->answer,
             fd_input, fd_output, rowbuffer);
 
     for (row = 0; row < rows; row++) {
 
         /* read in a row from each cell map */
-        G_percent(i, rows, 2);
+        G_percent(row, rows, 2);
 
         for (band = 0; band < 3; band++)
-            Rast_get_d_row(fd_input[band], rowbuffer[band], i);
+            Rast_get_d_row(fd_input[band], rowbuffer[band], row);
 
         /* process this row of the map */
-        rgb2his(rowbuffer, cols);
+        rgb2his(rowbuffer, cols, max_colors);
 
         /* write out the new row for each cell map */
         for (band = 0; band < 3; band++)
@@ -110,7 +130,7 @@ int main(int argc, char **argv)
     }
 
     /* progress */
-    G_percent(i, rows, 2);
+    G_percent(row, rows, 2);
 
     /* close output files */
     closefiles(opt_hue->answer, opt_intensity->answer, opt_saturation->answer,
