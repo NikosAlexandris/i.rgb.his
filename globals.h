f751574a9f1492255bbffc57930be7442a9f5ef3
Nikos Alexandris <nik@nikosalexandris.net>
2016-09-12 03:04:56 +0200

    modified input and their type for rgb2his

--- globals.h
+++ globals.h
@@ -11,6 +11,6 @@ void openfiles(char *, char *, char *, char *, char *, char *, int[3], int[3],
         DCELL *[3]);
 
 /* rgb2his.c */
-void rgb2his(DCELL *[3], int);
+void rgb2his(DCELL *[3], unsigned int, double);
 
 #endif /* __GLOBALS_H__ */
